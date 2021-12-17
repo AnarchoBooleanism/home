0d7176102110e66cfd99d2d52457aaaf97291153
Nathan Guerrero <naguerr2@uci.edu>
2021-12-17 15:10:38 -0800

    Change to buttons and CSS fix to proportions

--- src/Contact.js
+++ src/Contact.js
@@ -14,15 +14,20 @@ const Contact = () => {
               <div id="contactBox">
                 <h1 class="sectionHeader">Contact</h1>
                 <h2>Interested? Here you go:</h2>
-                <div id="emailBox">
-                  <a className="linkButton" id="emailButton" href="mailto:naguerr2@uci.edu">Email me!</a>
-                  <form><input type="email" value="naguerr2@uci.edu" readonly></input></form>
-                </div>
-                <div class="buttonWrapper">
-                  <a className="linkButton" id="resumeButton" href="">Resume</a>
-                  <a className="linkButton" href="https://github.com/AnarchoBooleanism"><i className="fab fa-github"></i> GitHub</a>
-                  <a className="linkButton" href="https://www.linkedin.com/in/nathan-guerrero-89a41919b/"><i className="fab fa-linkedin-in"></i> LinkedIn</a>
-                  <a className="linkButton" href="https://uci.joinhandshake.com/users/33173069">Handshake</a>
+                <div id="contactLinks">
+                  <div id="emailBox">
+                    <a className="linkButton" id="emailButton" href="mailto:naguerr2@uci.edu">Email me!</a>
+                    <a className="linkButton" id="resumeButton" href="">Resume</a>
+                  </div>
+                  <div class="buttonWrapper">
+                    <div id="emailCopier">
+                      <input id="emailCopyBox" type="email" value="naguerr2@uci.edu" readonly></input>
+                      <a id="emailCopyButton" onClick={() =>  navigator.clipboard.writeText('naguerr2@uci.edu')}><i class="far fa-copy"></i></a>
+                    </div>  
+                    <a className="linkButton" href="https://github.com/AnarchoBooleanism"><i className="fab fa-github"></i> GitHub</a>
+                    <a className="linkButton" href="https://www.linkedin.com/in/nathan-guerrero-89a41919b/"><i className="fab fa-linkedin-in"></i> LinkedIn</a>
+                    <a className="linkButton" href="https://uci.joinhandshake.com/users/33173069">Handshake</a>
+                  </div>
                 </div>
               </div>
             </div>
