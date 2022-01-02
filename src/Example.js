5f123e099e80a3e90bda38c597ce3fe6dd1c18d8
Nathan Guerrero <naguerr2@uci.edu>
2022-01-02 14:23:49 -0800

    Add second example to page

--- src/Example.js
+++ src/Example.js
@@ -3,9 +3,17 @@ import './style/Example.css';
 const Example = (props) => {
     const example = props.examples;
 
+    // Code to determine whether something is flipped
+    if (example.id % 2 === 0) {
+        var containerType = "flex-container flex-container--flipped";
+    }
+    else {
+        var containerType = "flex-container";
+    }
+
     return (
       <div className="example-container">
-        <div className="flex-container">
+        <div className={containerType}>
             <div>
                 <h1>{ example.title }</h1>
                 <h2>{ example.subtitle }</h2>
