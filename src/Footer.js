55e47beeb59695c71d6a60c73b0b6521bd13219c
Nathan Guerrero <naguerr2@uci.edu>
2022-05-24 14:03:00 -0700

    Updates to status

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
       <div className="footer-container">
         <div className="view-area footer-container__content">
-          <h2><a href="https://reactjs.org/" className="react-link"><i className="fab fa-react react-link__icon"></i>Made with React!</a> Copyright: Nathan Guerrero, 2021.</h2>
+          <h2><a href="https://reactjs.org/" className="react-link"><i className="fab fa-react react-link__icon"></i>Made with React!</a> Copyright: Nathan Guerrero, 2022.</h2>
         </div>
       </div>
     );
