d422b20acec450a750cf01926665c052de63bd3c
Nathan Guerrero <naguerr2@uci.edu>
2024-07-05 20:16:35 -0700

    Updates

---
+++ precache-manifest.3021845defc4d46528fb06b57c255dc9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d39ca29f6416092ab25606121d7e22a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "4378848e0675cdb597bd",
+    "url": "/home/static/css/main.983ae2ed.chunk.css"
+  },
+  {
+    "revision": "7dd5bb4908987298b8f7",
+    "url": "/home/static/js/2.c11c57e3.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/home/static/js/2.c11c57e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4378848e0675cdb597bd",
+    "url": "/home/static/js/main.b73867c1.chunk.js"
+  },
+  {
+    "revision": "43580c8c71aa406940a2",
+    "url": "/home/static/js/runtime-main.fc9e62d3.js"
+  },
+  {
+    "revision": "4a8ce5953d2333aa3c60119607573f4b",
+    "url": "/home/static/media/code_pic1.4a8ce595.jpg"
+  },
+  {
+    "revision": "b1b0afacab887bad72dd2eac4b47ad41",
+    "url": "/home/static/media/example_pic1.b1b0afac.png"
+  },
+  {
+    "revision": "13a45c956564245f27203683eca68dae",
+    "url": "/home/static/media/example_pic2.13a45c95.png"
+  },
+  {
+    "revision": "ca6172ec2afb1403358f70434fd5d0f7",
+    "url": "/home/static/media/face_pic1.ca6172ec.jpg"
+  }
+]);
