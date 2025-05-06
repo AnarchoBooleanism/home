2caa39a39aeca0be5c7bf1e3c8a3512b74ba7758
Nathan Guerrero <naguerr2@uci.edu>
2025-05-06 01:00:28 +0000

    Update basic info, styling

--- src/Contact.js
+++ src/Contact.js
@@ -17,14 +17,13 @@ const Contact = () => {
                 <h2 className="section-subheader">Interested? Here you go:</h2>
                 <div className="contact-info-container__links">
                   <div className="contact-info-container__links__priority-links">
-                    <a className="link-button link-button--email" href="mailto:naguerr2@uci.edu">Email me!</a>
+                    <div className="email-copier-container">
+                      <input className="email-copier-container__text" type="text" value="anarchbool@anarchbool.dev" readonly></input>
+                      <a className="email-copier-container__icon" onClick={() =>  navigator.clipboard.writeText('anarchbool@anarchbool.dev')}><i className="far fa-copy"></i></a>
+                    </div>  
                     <a className="link-button link-button--resume" href="">Resume</a>
                   </div>
                   <div className="button-wrapper button-wrapper--contact-info">
-                    <div className="email-copier-container">
-                      <input className="email-copier-container__text" type="email" value="naguerr2@uci.edu" readonly></input>
-                      <a className="email-copier-container__icon" onClick={() =>  navigator.clipboard.writeText('naguerr2@uci.edu')}><i className="far fa-copy"></i></a>
-                    </div>  
                     <a className="link-button" href="https://github.com/AnarchoBooleanism"><i className="fab fa-github"></i> GitHub</a>
                     <a className="link-button" href="https://www.linkedin.com/in/nathan-guerrero-89a41919b/"><i className="fab fa-linkedin-in"></i> LinkedIn</a>
                     <a className="link-button" href="https://uci.joinhandshake.com/users/33173069">Handshake</a>
--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
       <div className="footer-container">
         <div className="view-area footer-container__content">
-          <h2><a href="https://reactjs.org/" className="react-link"><i className="fab fa-react react-link__icon"></i>Made with React!</a> Copyright: Nathan Guerrero, 2024.</h2>
+          <h2><a href="https://reactjs.org/" className="react-link"><i className="fab fa-react react-link__icon"></i>Made with React!</a> Copyright: Nathan Guerrero, 2025.</h2>
         </div>
       </div>
     );
