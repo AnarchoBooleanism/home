3ae4675cbc96daf9946c72878a79144e8ede39d6
Nathan Guerrero <naguerr2@uci.edu>
2025-09-21 00:45:41 +0000

    Fix CD part of CI/CD in keywords list

--- src/keyword_list.js
+++ src/keyword_list.js
@@ -2,7 +2,7 @@
 const keywordList = [
     "Python", "C", "Java", "C++", "JavaScript", "React", "Node.js", "Bash", "Scripting",
     "Git", "Linux", "Debian", "Ubuntu", "Proxmox", "Docker", "Containerization", "Virtual Machines", "QEMU",
-    "Continuous Integration", "Continuous Deployment", "DevOps", "Networking", "Web Deployment", "Databases", "SQL",
+    "Continuous Integration", "Continuous Delivery", "DevOps", "Networking", "Web Deployment", "Databases", "SQL",
     "Search Engines", "Indexing", "Systems Development", "Systems Administration", "Automation", "Cybersecurity",
     "x86 Assembly", "Compilers", "APIs", "Multiprocessing", "Distributed Systems", "Software Design",
     "Object-Oriented Programming", "Functional Programming", "Full Stack Development", "Big Data",
