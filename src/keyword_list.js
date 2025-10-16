c2bf805542f0365ff271f5aa42ff4463e32c4ca4
Nathan Guerrero <naguerr2@uci.edu>
2025-10-16 23:43:43 +0000

    Widen view area and add a few new keywords

--- src/keyword_list.js
+++ src/keyword_list.js
@@ -1,12 +1,12 @@
 // List of keywords representing concepts, skills, etc., to be listed
 const keywordList = [
     "Python", "C", "Java", "C++", "JavaScript", "React", "Node.js", "Bash", "Scripting",
-    "Git", "Linux", "Debian", "Ubuntu", "Proxmox", "Docker", "Containerization", "Virtual Machines", "QEMU",
+    "Git", "Linux", "NixOS", "Debian", "Ubuntu", "Proxmox", "Docker", "Containerization", "Virtual Machines", "QEMU",
     "Continuous Integration", "Continuous Delivery", "DevOps", "Networking", "Web Deployment", "Databases", "SQL",
     "Search Engines", "Indexing", "Systems Development", "Systems Administration", "Automation", "Cybersecurity",
     "x86 Assembly", "Compilers", "APIs", "Multiprocessing", "Distributed Systems", "Software Design",
     "Object-Oriented Programming", "Functional Programming", "Full Stack Development", "Big Data",
-    "Remote Development", "SSH"
+    "Remote Development", "SSH", "Nginx"
 ];
 
 export default keywordList;
