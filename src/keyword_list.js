4e08fed39b08db059dffacb4ca07beba4e5c841a
Nathan Guerrero <naguerr2@uci.edu>
2025-11-14 02:07:36 +0000

    fix: Adjusting keywords list to be more organized and natural

--- src/keyword_list.js
+++ src/keyword_list.js
@@ -1,8 +1,8 @@
 // List of keywords representing concepts, skills, etc., to be listed
 const keywordList = [
-    "Python", "C", "Java", "C++", "JavaScript", "React", "Node.js", "Bash", "Scripting", "Ansible", "Nix", "Infrastructure as Code",
+    "Python", "C", "Java", "C++", "JavaScript", "React", "Node.js", "Bash", "Scripting", "Ansible",
     "Git", "Linux", "NixOS", "Debian", "Ubuntu", "Proxmox", "Docker", "Containerization", "Virtual Machines", "QEMU",
-    "Continuous Integration", "Continuous Delivery", "DevOps", "Networking", "Web Deployment", "Databases", "SQL",
+    "Continuous Integration", "Continuous Delivery", "Infrastructure as Code", "DevOps", "Networking", "Web Deployment", "Databases", "SQL",
     "Search Engines", "Indexing", "Systems Development", "Systems Administration", "Automation", "Cybersecurity",
     "x86 Assembly", "Compilers", "APIs", "Multiprocessing", "Distributed Systems", "Software Design",
     "Object-Oriented Programming", "Functional Programming", "Full Stack Development", "Big Data",
