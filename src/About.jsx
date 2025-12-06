fc041ec14521e2befd850df9bfa967e21bf859f5
Nathan Guerrero <naguerr2@uci.edu>
2025-12-06 11:07:22 +0000

    feat: Add animated underline and color changes to non-button links on hover/active

--- src/About.jsx
+++ src/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
               <p>Ever since childhood, creating and tinkering with software has been a passion of mine, a passion that has followed me into college and beyond. Throughout my journey, with my persistence and my attention to detail, I have gained a broad range of experience, from my time in a team hackathon to my times across numerous group projects.</p>
               <p>I am highly proficient in Python, C/C++, and Java, and have experience across the entire stack, from the front-end (React), to databases (SQL), to the systems-level. In addition, I like to compose/produce music and game in my free time. I also have an intermediate fluency in Spanish and Japanese.</p>
               <p>Currently based in San Diego, CA, having graduated from the University of California, Irvine in 2025.</p>
-              <p>Fun fact: this entire website was <a href="https://github.com/AnarchoBooleanism/home" target="_blank" rel="noopener noreferrer">written from scratch, with React, Vite, Sass, and Node.js!</a></p>
+              <p>Fun fact: this entire website was <a href="https://github.com/AnarchoBooleanism/home" target="_blank" rel="noopener noreferrer" className="link">written from scratch, with React, Vite, Sass, and Node.js!</a></p>
             </div>
           </div>
         </div>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
       <div className="footer-container">
         <div className="view-area footer-container__content">
-          <h2><a href="https://github.com/AnarchoBooleanism/home" target="_blank" rel="noopener noreferrer" className="react-link"><i className="fab fa-react react-link__icon"></i>Made with React!</a> Copyright: Nathan Guerrero, 2025.</h2>
+          <h2><a href="https://github.com/AnarchoBooleanism/home" target="_blank" rel="noopener noreferrer" className="react-link link"><i className="fab fa-react react-link__icon"></i>Made with React!</a> Copyright: Nathan Guerrero, 2025.</h2>
         </div>
       </div>
     );
