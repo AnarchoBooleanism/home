bfeec95a1d10645c80528e8a90e9d6af23040b04
Nathan Guerrero <naguerr2@uci.edu>
2026-01-20 05:24:05 +0000

    change: Add italics in about section to part about actively looking for work

--- src/About.jsx
+++ src/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
               <p>Starting from childhood, creating and tinkering with software has been a passion of mine, one that has followed me into college and beyond. Throughout my journey, with my persistence and my attention to detail, I have gained a broad range of experience, including in team hackathons and numerous group projects.</p>
               <p>I am strongly proficient in Python, C/C++, and Java, with experience across the entire stack, from React to SQL. More importantly than any technology, I have the ability to quickly learn new skills and languages, as well as break down any complex problem into concise and idiomatic solutions.</p>
               <p>In my free time, I like to game, compose/produce music, and work on my homelab, the Sapphic Homelab/Home Server, daily. Additionally, I have an intermediate fluency in both Spanish and Japanese.</p>
-              <p>Currently based in San Diego, CA, having graduated from the University of California, Irvine in 2025, with a B.S. in Computer Science. At the moment, I am looking for work!</p>
+              <p>Currently based in San Diego, CA, having graduated from the University of California, Irvine in 2025, with a B.S. in Computer Science. <i>At the moment, I am looking for work!</i></p>
               <p>Fun fact: this entire website was <a href="https://github.com/AnarchoBooleanism/home" target="_blank" rel="noopener noreferrer" className="link">written from scratch, with React, Vite, Sass, and Node.js!</a></p>
             </div>
           </div>
