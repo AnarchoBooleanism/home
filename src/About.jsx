d16904dfd2c5a02223dc4a8dbb098f982539486f
Nathan Guerrero <naguerr2@uci.edu>
2026-02-05 04:52:50 +0000

    update: Tweak ordering of personal titles in about section

--- src/About.jsx
+++ src/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
             </div>
             <div>
               <h1 className="section-header">About Me (Hello!)</h1>
-              <h2 className="section-subheader">I am Nathan Guerrero, a software developer and tinkerer, focused on DevOps and the back-end.</h2>
+              <h2 className="section-subheader">I am Nathan Guerrero, a tinkerer and software developer, focused on DevOps and the back-end.</h2>
               <p>Starting from childhood, creating and tinkering with software has been a passion of mine, one that has followed me into college and beyond. Throughout my journey, with my persistence and my attention to detail, I have gained a broad range of experience, including in team hackathons and numerous group projects.</p>
               <p>I am strongly proficient in Python, C/C++, and Java, with experience across the entire stack, from React to SQL. More importantly than any technology, I have the ability to quickly learn new skills and languages, as well as break down any complex problem into concise and idiomatic solutions.</p>
               <p>In my free time, I like to game, compose/produce music, and work on my homelab, the Sapphic Homelab/Home Server, daily. Additionally, I have an intermediate fluency in both Spanish and Japanese.</p>
