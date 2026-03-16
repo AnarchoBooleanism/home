1651e36162d6261646be576e5dd317507286346c
Nathan Guerrero <naguerr2@uci.edu>
2026-03-16 00:13:42 +0000

    update: Add FastAPI as keyword

--- src/keyword_list.js
+++ src/keyword_list.js
@@ -3,7 +3,7 @@ const keywordList = [
     "Python", "C", "Java", "C++", "JavaScript", "React", "Node.js", "Bash", "Scripting", "Ansible",
     "Git", "Linux", "NixOS", "Debian", "Ubuntu", "Proxmox", "Docker", "Containerization", "Virtual Machines", "QEMU",
     "Continuous Integration", "Continuous Delivery", "Infrastructure as Code", "DevOps", "GitOps", "Networking", "Web Deployment", "Databases", "SQL",
-    "Search Engines", "Indexing", "Systems Development", "Systems Administration", "Automation", "Cybersecurity",
+    "FastAPI", "Search Engines", "Indexing", "Systems Development", "Systems Administration", "Automation", "Cybersecurity",
     "x86 Assembly", "Compilers", "APIs", "Multiprocessing", "Distributed Systems", "Software Design",
     "Object-Oriented Programming", "Functional Programming", "Full Stack Development", "Big Data",
     "Remote Development", "SSH", "Nginx"
