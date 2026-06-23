637efedaf621a265230452d4e562e969f603018a
Nathan Guerrero <naguerr2@uci.edu>
2026-06-22 21:45:12 -0700

    change: Update keyword list to reflect DevOps focus and new skills

--- src/keyword_list.js
+++ src/keyword_list.js
@@ -1,12 +1,14 @@
 // List of keywords representing concepts, skills, etc., to be listed
 const keywordList = [
-    "Python", "C", "Java", "C++", "JavaScript", "React", "Node.js", "Bash", "Scripting", "Ansible",
-    "Git", "Linux", "NixOS", "Debian", "Ubuntu", "Proxmox", "Docker", "Containerization", "Virtual Machines", "QEMU",
-    "Continuous Integration", "Continuous Delivery", "Infrastructure as Code", "DevOps", "GitOps", "Networking", "Web Deployment", "Databases", "SQL",
-    "FastAPI", "Search Engines", "Indexing", "Systems Development", "Systems Administration", "Automation", "Event-Driven Architecture", "Cybersecurity",
+    "Python", "C", "Java", "C++", "JavaScript", "React", "Node.js", "Bash", "Scripting", "Git",
+    "Docker", "Kubernetes", "Microservices", "Containerization", "Continuous Integration", "Continuous Delivery", "GitHub Actions",
+    "Infrastructure as Code", "Automation", "DevOps", "DevSecOps", "GitOps", "Event-Driven Architecture", "Load Balancers", "Traefik", "Nginx",
+    "Linux", "NixOS", "Ansible", "Debian", "Ubuntu", "Proxmox",
+    "Virtual Machines", "QEMU", "Networking", "Web Deployment", "Databases", "SQL",
+    "FastAPI", "Search Engines", "Indexing", "Systems Development", "Systems Administration", "Cybersecurity",
     "x86 Assembly", "Compilers", "APIs", "Multiprocessing", "Distributed Systems", "Software Design",
     "Object-Oriented Programming", "Functional Programming", "Full Stack Development", "Big Data",
-    "Remote Development", "SSH", "Nginx"
+    "AI" // Might as well
 ];
 
 export default keywordList;
