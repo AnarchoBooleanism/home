94191dfd36f9a07d1f1ce407dd045ba534c1ab46
Nathan Guerrero <naguerr2@uci.edu>
2026-08-05 16:23:17 -0700

    update: Add cloud computing keyword

--- src/keyword_list.js
+++ src/keyword_list.js
@@ -1,7 +1,7 @@
 // List of keywords representing concepts, skills, etc., to be listed
 const keywordList = [
     "Python", "C", "Java", "C++", "JavaScript", "React", "Node.js", "Bash", "Scripting", "Git",
-    "Docker", "Kubernetes", "SOPS", "HashiCorp Vault", "Microservices", "Containerization", "Continuous Integration", "Continuous Delivery", "GitHub Actions",
+    "Docker", "Kubernetes", "Cloud Computing", "SOPS", "HashiCorp Vault", "Microservices", "Containerization", "Continuous Integration", "Continuous Delivery", "GitHub Actions",
     "Infrastructure as Code", "Secrets Management", "Automation", "DevOps", "DevSecOps", "GitOps", "Event-Driven Architecture", "Load Balancers", "Traefik", "Nginx",
     "Linux", "NixOS", "Ansible", "Debian", "Ubuntu", "Proxmox",
     "Virtual Machines", "QEMU", "Networking", "Web Deployment", "Databases", "SQL",
